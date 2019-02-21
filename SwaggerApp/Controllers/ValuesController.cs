805dd8e6d97a9e5a2d63c9c2428900fb40ed19f3
Karina Lisniak <karyna.lisniak@gmail.com>
2019-02-21 23:08:26 +0200

    Initial Commit

---
+++ SwaggerApp/Controllers/ValuesController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Reflection;
+
+namespace SwaggerApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        /// <summary>
+        /// Get current Assembly name
+        /// </summary>
+        /// <remarks>
+        /// Sample request:
+        /// GET
+        /// {
+        ///     "id": 1
+        /// }
+        /// </remarks>
+        /// <returns>Name of current Assembly</returns>
+        /// <response code="200">Success</response>
+        /// <response code="404">Not found</response>
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return Assembly.GetExecutingAssembly().GetName().Name;
+        }
+    }
+}
